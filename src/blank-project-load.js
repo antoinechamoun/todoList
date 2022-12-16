b9f955346dd2c3d5ab884ffa810ae34e5e2666be
Tony <tonychamoun6@gmail.com>
2022-12-16 14:59:59 +0200

    Restart

--- src/blank-project-load.js
+++
@@ -1,7 +0,0 @@
-
-export const blankProjectLoad = () => {
-    let projectsArray =[]
-    let projectTitle="Default Project"
-    projectsArray.push({projectTitle})
-    return{projectsArray, projectTitle}
-}
--- src/create-to-do.js
+++
@@ -1,6 +0,0 @@
-let toDoArray = []
-
-export const createToDo = (title, desc, dueDate, priority, checklist) => {
-    toDoArray.push({title, desc, dueDate, priority, checklist})
-    return{title, desc, dueDate, priority, checklist}
-}
